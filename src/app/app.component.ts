b35e4f9ca396c4ca6f2b7ccf886fb6b23c3a937c
Neile23 <aurelien.wi@gmail.com>
2023-03-23 11:10:16 +0100

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
 
 interface User {
   id: number;
@@ -15,4 +16,8 @@ interface User {
 })
 
 export class AppComponent {
+
+  constructor(private http: HttpClient, private titleService: Title) {
+    this.titleService.setTitle('BookWorm');
+  }
 }
